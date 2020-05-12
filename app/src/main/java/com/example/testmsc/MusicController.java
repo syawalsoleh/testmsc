c33fd23f99c4b53dbb952eede83263e49a4e2f29
Syawaludin Soleh <soleh@createch.id>
2020-05-12 23:36:03 +0700

    first

---
+++ app/src/main/java/com/example/testmsc/MusicController.java
@@ -0,0 +1,11 @@
+package com.example.testmsc;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+    public void hide(){}
+}
